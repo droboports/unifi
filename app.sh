2aa91eff6ecd6c99c777626015da5df62e7023db
Ricardo Padilha <ricardospadilha@gmail.com>
2015-05-17 20:24:22 -0700

    First version, based on 3.2.10

---
+++ app.sh
@@ -0,0 +1,34 @@
+### UNIFI ###
+_build_unifi() {
+local VERSION="3.2.10"
+local FOLDER="UniFi"
+local FILE="${FOLDER}.unix.zip"
+local URL="http://www.ubnt.com/downloads/unifi/${VERSION}/${FILE}"
+
+_download_file_in_folder "${FILE}" "${URL}" "${VERSION}"
+mkdir -p "target"
+if [ -d "target/${FOLDER}" ]; then rm -vfr "target/${FOLDER}"; fi
+unzip "download/${VERSION}/${FILE}" -d "${PWD}/target"
+mkdir -p "${DEST}/data"
+cp -vafr "target/${FOLDER}/"* "${DEST}/"
+}
+
+### MONGODB ###
+_build_mongodb() {
+local VERSION="3.0.2"
+local FOLDER="mongodb3"
+local FILE="mongod"
+local URL="https://github.com/droboports/${FOLDER}/releases/download/v${VERSION}/${FILE}"
+
+_download_file_in_folder "${FILE}" "${URL}" "${DROBO}"
+if [ ! -d "${DEST}/bin" ]; then mkdir -p "${DEST}/bin"; fi
+if [   -h "${DEST}/bin/${FILE}" ]; then rm -vf "${DEST}/bin/${FILE}"; fi
+cp -vaf "download/${DROBO}/${FILE}" "${DEST}/bin/"
+chmod a+x "${DEST}/bin/${FILE}"
+}
+
+_build() {
+  _build_unifi
+  _build_mongodb
+  _package
+}
---
+++ src/dest/service.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env sh
+#
+# UniFi AP Controller service
+
+# import DroboApps framework functions
+. /etc/service.subr
+
+framework_version="2.1"
+name="unifi"
+version="3.2.10"
+description="UniFi AP Controller"
+depends="java8"
+webui=":8043/"
+
+prog_dir="$(dirname "$(realpath "${0}")")"
+tmp_dir="/tmp/DroboApps/${name}"
+pidfile="${tmp_dir}/pid.txt"
+logfile="${tmp_dir}/log.txt"
+statusfile="${tmp_dir}/status.txt"
+errorfile="${tmp_dir}/error.txt"
+daemon="${DROBOAPPS_DIR}/java8/bin/java"
+
+# backwards compatibility
+if [ -z "${FRAMEWORK_VERSION:-}" ]; then
+  . "${prog_dir}/libexec/service.subr"
+fi
+
+start() {
+  export LANG="C"
+  export LC_ALL="C"
+  cd "${prog_dir}"
+  setsid "${daemon}" -jar "${prog_dir}/lib/ace.jar" start &
+  local _pid=$!
+  if [ ${_pid} -gt 0 ]; then
+    echo "${_pid}" > "${pidfile}"
+  fi
+}
+
+stop() {
+  export LANG="C"
+  export LC_ALL="C"
+  "${daemon}" -jar "${prog_dir}/lib/ace.jar" stop
+}
+
+force_stop() {
+  export LANG="C"
+  export LC_ALL="C"
+  start-stop-daemon -K -s 9 -p "${pidfile}" -x "${daemon}" -q
+  "${prog_dir}/bin/mongod" --data "${prog_dir}/data/db" --shutdown
+}
+
+# boilerplate
+if [ ! -d "${tmp_dir}" ]; then mkdir -p "${tmp_dir}"; fi
+exec 3>&1 4>&2 1>> "${logfile}" 2>&1
+STDOUT=">&3"
+STDERR=">&4"
+echo "$(date +"%Y-%m-%d %H-%M-%S"):" "${0}" "${@}"
+set -o errexit  # exit on uncaught error code
+set -o nounset  # exit on unset variable
+set -o pipefail # propagate last error code on pipe
+set -o xtrace   # enable script tracing
+
+main "${@}"
